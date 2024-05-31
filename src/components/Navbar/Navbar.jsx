b6bf634ac0a16f07524bb914e7cd72a01433f4a9
Angad Basak <angadbasak.code@gmail.com>
2024-05-31 21:39:50 +0600

    first iteration

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+
+export const Navbar = () => {
+  return (
+  <><nav className={styles.navbar}>
+      <a href="/">Portfolio</a>
+    </nav>
+      <div className={styles.menu}>
+        <ul className={styles.menuItems}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div></>
+  )
+}
